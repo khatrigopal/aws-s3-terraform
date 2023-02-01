728465628ea23635651bf82b948a7e26120228b5
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-01 13:10:13 +0530

    Update variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -39,3 +39,27 @@ variable "s3_restrict_public_buckets" {
   type        = bool
   default     = true
 }
+
+################### ECR ###############
+
+variable "ecr_name" {
+  description = "The list of ecr names to create"
+  type        = list(string)
+  default     = null
+}
+variable "tags" {
+  description = "The key-value maps for tagging"
+  type        = map(string)
+  default     = {}
+}
+variable "image_mutability" {
+  description = "Provide image mutability"
+  type        = string
+  default     = "MUTABLE"
+}
+
+variable "encrypt_type" {
+  description = "Provide type of encryption here"
+  type        = string
+  default     = "KMS"
+}
