7901278b33e253bde4767b890af5cca163c3bb95
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-01 12:24:30 +0530

    Update variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,3 +1,21 @@
 variable "bucket_name" {
   description = "The AWS region to use to create resources."
   }
+
+variable "acl" {
+  type        = string
+  description = " Defaults to private "
+  default     = "private"
+}
+
+variable "bucket_force_destroy" {
+  description = ""
+  type        = bool
+  default     = "false"
+}
+
+variable "versioning" {
+  description = "versioning config"
+  type        = string
+  default     = "Enabled"
+}
