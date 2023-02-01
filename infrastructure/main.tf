f131383939e505e666e7d9da3d8a2184ee28e98a
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-01 13:09:40 +0530

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -21,4 +21,13 @@ module "s3" {
   versioning = "${var.versioning}"
   acl        = "${var.s3_acl}"
 }
- 
+
+  ############## ECR ###################
+  
+  module "ecr-repo" {
+  source           = "./../modules/ecr"
+  ecr_name         = var.ecr_name
+  tags             = var.tags
+  image_mutability = var.image_mutability
+
+}
